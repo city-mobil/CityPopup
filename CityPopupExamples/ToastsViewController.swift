28b4b25415361fb3f11e42169c10a52788594061
Georgiy Sabanov <g.sabanov@city-mobil.ru>
2020-11-24 18:10:52 +0300

    Добавил второй тост

--- CityPopupExamples/ToastsViewController.swift
+++ CityPopupExamples/ToastsViewController.swift
@@ -64,6 +64,14 @@ extension ToastsViewController {
         leadingImageView.layer.cornerRadius = 8
         toastView.add(leadingView: leadingImageView, width: 32, height: 32)
 
+        // Add actions for toast
+        toastView.tapHandler = { [weak self] in
+            self?.showToastWithBottomAlignmentAndAutodismiss()
+            toastView.hide(usingAnimator: CPSlideAnimator(direction: .down))
+        }
+        
+        // Add swipe to hide recognition to toast
+        toastView.setHideSwipeGestureEnabled()
                 
         // Show the toast using the animator with sliding up animation.
         // Specify margins to a container of the toast.
@@ -77,6 +85,55 @@ extension ToastsViewController {
             animator: CPSlideAnimator(direction: .down),
             attributes: .init(
                 position: .top,
+                margins: .init(top: 24, left: 24, bottom: 24, right: 24)
+            )
+        )
+    }
+    
+    private func showToastWithBottomAlignmentAndAutodismiss() {
+        // Create the toast view instance with title, message and leading view.
+        // Notes:
+        // - The `style` parameter has a lot of settings to customize the toast view;
+        let toastView = CPToastView(
+            title: "Second toast.",
+            message: "This toast is showing the leading view, title and message. It is aligned at the bottom of the screen and autodismisses in 3 seconds. Text alignment is center",
+            style: .init(
+                titleTextAlignment: .center,
+                titleNumberOfLines: 2,
+                messageTextAligment: .center,
+                messageNumberOfLines: 6,
+                verticalSpacingAfterTitle: 4
+            )
+        )
+        
+        // Add leading view with some image.
+        let leadingImageView = UIImageView(image: #imageLiteral(resourceName: "Cover"))
+        leadingImageView.contentMode = .scaleAspectFill
+        leadingImageView.clipsToBounds = true
+        leadingImageView.layer.cornerRadius = 8
+        toastView.add(leadingView: leadingImageView, width: 32, height: 32)
+        
+        // Add actions for toast
+        toastView.tapHandler = {
+            
+        }
+        
+        // Add swipe to hide recognition to toast with '.down' direction
+        toastView.setHideSwipeGestureEnabled(withDirection: .down)
+        
+        // Show the toast using the animator with sliding up animation.
+        // Specify margins to a container of the toast.
+        // Notes:
+        // - The animator is using not only to show a popup but to hide by default;
+        // - CPSlideAnimator has another parameters for init, feel free to use it on need;
+        // - CPSlideAnimator will hide the popup with reverse direction by default;
+        // - The `attributes` parameter has a lot of settings to customize show operation.
+        CityPopup.shared.show(
+            popup: toastView,
+            animator: CPSlideAnimator(direction: .up),
+            attributes: .init(
+                autodismissDelay: 3,
+                position: .bottom,
                 margins: .init(top: 24, left: 24, bottom: 24, right: 24)
             )
         )
